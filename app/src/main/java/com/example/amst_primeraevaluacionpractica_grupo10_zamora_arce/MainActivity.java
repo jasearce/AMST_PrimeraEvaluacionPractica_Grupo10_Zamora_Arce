5a880eab6d206a5a9f0fa6b4500f502e34597b0a
Javier Arce <jasearce@espol.edu.ec>
2019-11-29 09:19:46 -0500

    creacion primera pantalla

--- app/src/main/java/com/example/amst_primeraevaluacionpractica_grupo10_zamora_arce/MainActivity.java
+++ app/src/main/java/com/example/amst_primeraevaluacionpractica_grupo10_zamora_arce/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.amst_primeraevaluacionpractica_grupo10_zamora_arce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnEmp;
+    private TextView nameApp, autores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEmp = (Button)findViewById(R.id.btn_empezar);
+        nameApp = (TextView) findViewById(R.id.txt_app);
+        autores = (TextView)findViewById(R.id.txt_autores);
+    }
+
+    public void pasarAPreguntas(View view){
+        Intent intent = new Intent(this, Preguntas.class);
+        startActivity(intent);
     }
 }
